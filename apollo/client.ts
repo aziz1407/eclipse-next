e477b50cdf7745627cc4d7c12a34c44e330a3eec
aziz1407 <aibrokhimov2003@gmail.com>
2025-06-26 17:31:21 +0900

    feat: client.ts

--- apollo/client.ts
+++ apollo/client.ts
@@ -76,7 +76,7 @@ function createIsomorphicLink() {
 
 		// @ts-ignore
 		const link = new createUploadLink({
-			uri: process.env.REACT_APP_API_GRAPHQL_URL,
+			uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
 		});
 
 		/* WEBSOCKET SUBSCRIPTION LINK */
